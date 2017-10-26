f6f8a75b436d079739de366c174d08f14c51a6de
Nguyen Dinh Quang <nguoikiemtim@gmail.com>
2017-10-26 19:18:54 +0700

    try to add marker

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -9,36 +9,68 @@ import { MapsAPILoader, GoogleMapsAPIWrapper } from '@agm/core';
 })
 export class AppComponent implements OnInit {
   title = 'My first AGM project';
-  lat = 51.678418;
-  lng = 6.809007;
+  lat = 10.729190;
+  lng = 106.721715;
+  agmBounds: google.maps.LatLngBounds;
+  zoom = 15;
+  markers: marker[] = [
+  ];
 
-  private _wrapper: GoogleMapsAPIWrapper;
   ngOnInit() {}
-  constructor(private mapsAPILoader: MapsAPILoader) {
+  constructor(private mapsAPILoader: MapsAPILoader,
+              private wrapper: GoogleMapsAPIWrapper) {}
 
-      }
+  DisplayBank(): void {
 
-  myClickFunc(): void {
+    this.wrapper.getBounds().then(x => {
+      console.log('aaaa:');
+      console.log(x);
+    });
+
     this.mapsAPILoader.load().then(() => {
 
       const opt: google.maps.MapOptions = {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+        center: {lat: this.lat, lng: this.lng},
+        zoom: this.zoom,
       };
+
       const mapDiv: HTMLDivElement = <HTMLDivElement>document.getElementById('ggmap');
+
       const m = new google.maps.Map(mapDiv, opt);
       const placeService =  new google.maps.places.PlacesService(mapDiv);
       const request: google.maps.places.PlaceSearchRequest = {
-        location: {lat: -33.867, lng: 151.195},
-        radius: 500,
-        type: 'store',
+        location: {lat: this.lat, lng: this.lng},
+        bounds: this.agmBounds,
+        type: 'bank',
       };
-      placeService.nearbySearch(request, (result, response) => {
-        console.log(result);
+      console.log(request);
+      placeService.nearbySearch(request, (results, status) => {
+        console.log(results);
+        this.PushMarkers(results);
       });
-
     });
-
   }
+
+  PushMarkers(marker_arg: google.maps.places.PlaceResult[]): void {
+    for (const place_iterator of marker_arg) {
+        const new_marker: marker = {
+        draggable: false,
+        lat: place_iterator.geometry.location.lat(),
+        lng: place_iterator.geometry.location.lng(),
+      };
+      this.markers.push(new_marker);
+    }
+  }
+  boundsChange(event) {
+    this.agmBounds = event;
+  }
+
+}
+// just an interface for type safety.
+interface marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
 }
 
